b1ddb515e4127d062ce58ae86634647e937fb824
Micah Hillman <113869019+headunderheels@users.noreply.github.com>
2024-01-22 00:58:41 -0500

    Update spatial_audio.js

--- js/spatial_audio.js
+++ js/spatial_audio.js
@@ -2,7 +2,7 @@ const sound = new Howl({
   src: ['audio/heavy\ pad.mp3'],
   spatial: true,
   loop: true,
-  volume: 2.6,
+  volume: 1,
   pos: [0,0,0],
   pan: 0,
   panningModel: 'HRTF' // Set panning model to HRTF
@@ -19,8 +19,8 @@ function toggleAudio() {
 document.addEventListener('mousemove', function(event) {
         const phi = (-event.clientX / window.innerWidth) * Math.PI + Math.PI
         const z = -(event.clientY / window.innerHeight) * 2 + 1/2;
-        const rho = 3;
-        sound.pos(rho * Math.cos(phi), rho *  Math.sin(phi), z); // Update audio source position using cylindrical polar coordinates
+        const rho = 1;
+        sound.pos(rho * Math.cos(phi), rho *  Math.sin(phi+Math.PI), z); // Update audio source position using cylindrical polar coordinates
         // console.log('L/R Position:', phi); // as expected (pi -> pi/2 -> 0)
         console.log('Object Height:', z);
 });
